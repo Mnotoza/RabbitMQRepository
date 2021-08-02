333fc831260403bc16e5f1094b810b5fce0f8ea5
Mnotoza <33994742+Mnotoza@users.noreply.github.com>
2021-08-02 23:22:47 +0200

    Committing an assessment test

---
+++ TransmitMessageRabbitMQ/SendMessage.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace TransmitMessageRabbitMQ
+{
+   class SendMessage
+   {
+      public static void Main(string[] args)
+      {
+         var factory = new ConnectionFactory() {HostName = "localhost"};
+         using (var connection = factory.CreateConnection())
+         {
+            using (var channel = connection.CreateModel())
+            {
+               channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false,
+                  arguments: null);
+
+               Console.WriteLine("Mfundiso");
+               string name = "Mfundiso";
+               string message = string.Format("Hello my name is,{0}", name);
+               var body = Encoding.UTF8.GetBytes(message);
+
+               channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+               Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+         }
+      }
+   }
+}
